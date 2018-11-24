2ffd45052db6ddfff563bb18be16d10b5ff9ba76
David Streid <dcs2153@columbia.edu>
2018-11-24 08:36:36 -0600

    showing month string

--- app/src/appointmentTrackerComponent/calendarDateComponent/calendar-date.component.ts
+++ app/src/appointmentTrackerComponent/calendarDateComponent/calendar-date.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnChanges } from '@angular/core';
-
+import DateUtil from '../../utils/date.util';
 
 @Component({
   selector: 'calendar-date',
@@ -12,6 +12,16 @@ export class CalendarDateComponent implements OnChanges  {
   month: number;
   @Input()
   day: number;
+
+  private dateUtil;
+
+  constructor() {
+    this.dateUtil = new DateUtil();
+  }
+
+  displayMonth(month: number): string {
+    return this.dateUtil.getMonthString( month );
+  }
 
   ngOnChanges(){
     console.log(this.month);
