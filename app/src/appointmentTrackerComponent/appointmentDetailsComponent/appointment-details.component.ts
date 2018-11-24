6ff01dcab505c40c1ee36051a875f704968bf22e
David Streid <dcs2153@columbia.edu>
2018-11-24 08:36:19 -0600

    passing appointment object

--- app/src/appointmentTrackerComponent/appointmentDetailsComponent/appointment-details.component.ts
+++ app/src/appointmentTrackerComponent/appointmentDetailsComponent/appointment-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'appointment-details',
@@ -6,11 +6,20 @@ import { Component, Input } from '@angular/core';
   styleUrls: [ 'appointment-details.component.scss' ]
 })
 
-export class AppointmentDetailsComponent {
+export class AppointmentDetailsComponent implements OnChanges {
   @Input()
+  public appointment: object;
+
   public day: number;
-  @Input()
   public month: number;
-  @Input()
   public address: object;
+  public place: object;
+  public title: string;
+
+  ngOnChanges() {
+    this.day = this.appointment['date']['day'];
+    this.month = this.appointment['date']['month'];
+    this.title = this.appointment['date']['title'];
+    this.place = this.appointment['date']['place'];
+  }
 }
