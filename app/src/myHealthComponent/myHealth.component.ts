9807a3b83c07001fb0845e7af85ec20d3e4e072b
David Streid <dcs2153@columbia.edu>
2018-11-26 19:02:00 -0600

    Adding prescription and doctors to myHealth

--- app/src/myHealthComponent/myHealth.component.ts
+++ app/src/myHealthComponent/myHealth.component.ts
@@ -6,4 +6,50 @@ import { Component } from '@angular/core';
   templateUrl: 'MyHealth.component.html'
 })
 
-export class MyHealthComponent{}
+export class MyHealthComponent{
+  public prescriptions: Object[];
+  public doctors: Object[];
+
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.prescriptions = this.getPrescriptions();
+    this.doctors = this.getDoctors();
+  }
+
+  getDoctors(): Object[] {
+    // TODO - Replace w/ service call
+    const doctors = [
+      {
+        name: 'Eric Toig',
+        type: 'Primary Care'
+      },
+      {
+        name: 'House',
+        type: 'Oncologist'
+      }
+    ];
+
+    return doctors;
+  }
+
+  getPrescriptions(): Object[] {
+    // TODO - implment service call
+    const prescriptions = [
+      {
+        name: 'MultiVitamin',
+        qty: 1,
+        frequency: 'daily'
+      },
+      {
+        name: 'NicodermCQ',
+        qty: 1,
+        frequency: 'daily'
+      }
+    ];
+
+    return prescriptions;
+  }
+}
