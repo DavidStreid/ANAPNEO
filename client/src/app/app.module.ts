89746277f6203ac6d4d45f06458c2f060518eddc
David Streid <dcs2153@columbia.edu>
2018-12-06 16:05:47 -0500

    removing unnecessary browserModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
@@ -13,7 +12,6 @@ import { ɵa as AnapneoModule } from 'anapneo-lib';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AnapneoModule
   ],
   exports: [],
