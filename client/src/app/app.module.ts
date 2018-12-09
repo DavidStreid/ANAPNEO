1643108d9f258634978ef2b7f073f4169760b12c
David Streid <dcs2153@columbia.edu>
2018-12-09 12:05:38 -0500

    Adding browser module to client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,20 @@
+import { BrowserModule }                          from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 // Loads dynamically
-// import { AnapneoModule } from '../../projects/anapneo-lib/src/anapneo/anapneo.module';
+import { AnapneoModule } from '../../projects/anapneo-lib/src/anapneo/anapneo.module';
 
 // Loads from library
-import { ɵa as AnapneoModule } from 'anapneo-lib';
+// import { AnapneoModule } from 'anapneo-lib';
 
 @NgModule({
+  imports: [
+    AnapneoModule,
+    BrowserModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    AnapneoModule
   ],
   exports: [],
   providers: [],
