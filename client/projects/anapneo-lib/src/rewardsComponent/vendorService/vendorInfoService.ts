097200db4a3853cb4d9389943f4115cf96cfa9b0
David Streid <dcs2153@columbia.edu>
2018-12-15 14:49:19 -0500

    ResponseHandlerUtil implementation

--- client/projects/anapneo-lib/src/rewardsComponent/vendorService/vendorInfoService.ts
+++ client/projects/anapneo-lib/src/rewardsComponent/vendorService/vendorInfoService.ts
@@ -4,17 +4,27 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of'
 import { map, catchError } from 'rxjs/operators';
 
+import ResponseHandlerUtil from '../../utils/services/responseHandler.util';
+
 @Injectable()
 export class VendorInfoService {
   private loggingEnabled: boolean = true;
-  constructor(private http: HttpClient){}
+  private responseHandlerUtil: ResponseHandlerUtil;
 
+  constructor(private http: HttpClient){
+    this.responseHandlerUtil = new ResponseHandlerUtil();
+  }
+
+  // TODO - Add correct return types
   public getVendors(userId: String) {
     if( this.loggingEnabled ) console.log( "VendorInfoService::getVendors" );
 
     const url = "http://localhost:4300/vendors?userId=" + userId;
     return this.http.get(url).pipe(
-      map((response: HttpResponseBase) => { return response }),
-      catchError((error: HttpErrorResponse ) =>  of(`Error: ${error}`)));
+      map((response: HttpResponseBase) => {
+        console.log('Successful getVendors request');
+        return response
+      }),
+      catchError((error: HttpErrorResponse ) => this.responseHandlerUtil.handleError(error)));
   }
 }
--- client/projects/anapneo-lib/src/services/login.service.ts
+++ client/projects/anapneo-lib/src/services/login.service.ts
@@ -3,21 +3,29 @@ import { HttpClient, HttpResponseBase, HttpErrorResponse } from '@angular/common
 import { Observable } from 'rxjs/Observable';
 import { map, catchError } from 'rxjs/operators';
 
+import ResponseHandlerUtil from '../utils/services/responseHandler.util';
+
 @Injectable()
 export class LoginService {
   private loggingEnabled: boolean = false;
 
-  constructor(private http: HttpClient){}
+  private responseHandlerUtil: ResponseHandlerUtil;
 
-  login(userId: String, pwd: String) {
+  constructor(private http: HttpClient){
+    this.responseHandlerUtil = new ResponseHandlerUtil();
+  }
+
+  login(userId: String, pwd: String) : Observable<HttpResponseBase>{
     if( this.loggingEnabled ) console.log( "loginService::login" );
 
     const url = "http://localhost:4300/login";
     const body = { userId, pwd };
 
     return this.http.post( url, body ).pipe(
-      map((response: HttpResponseBase) => {return response}),
-      catchError((error: HttpErrorResponse ) => Observable.throw(error))
-    );
+      map((response: HttpResponseBase) => {
+        console.log('Successful login');
+        return response
+      }),
+      catchError((error: HttpErrorResponse ) => this.responseHandlerUtil.handleError(error)));
   }
 }
