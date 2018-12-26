3a49b7b807aca95f28bac5956211c4fc2b9d24df
David Streid <dcs2153@columbia.edu>
2018-12-25 18:02:02 -0600

    Pushing src db script

---
+++ src/db/db.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var state = {
+  db: null
+}
+
+exports.connect = function(url, done) {
+  if (state.db) return done()
+
+  mongoose.connect(url, function(err, db) {
+    if (err) return done(err);
+
+    console.log("Connected to Mongo");
+
+    state.db = db;
+    done()
+  })
+}
+
+exports.get = function() {
+  return state.db;
+}
+
+exports.close = function(done) {
+  if (state.db) {
+    state.db.close(function(err, result) {
+      state.db = null
+      state.mode = null
+      done(err)
+    })
+  }
+}
