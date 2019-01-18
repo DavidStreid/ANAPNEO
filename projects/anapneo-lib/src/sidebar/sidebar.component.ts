3da7033533c9b704b6cce54dc67bc247e0a0fec6
David Streid <dcs2153@columbia.edu>
2019-01-18 02:40:29 -0500

    Anapneo to HealthAdv switch

--- projects/anapneo-lib/src/sidebar/sidebar.component.ts
+++ projects/anapneo-lib/src/sidebar/sidebar.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: [ 'sidebar.component.scss' ],
 })
 export class SideBarComponent {
-  public descriptionTest: String = "Anapneo is an app-based rewards program to improve lung cancer prognosis.";
+  public descriptionTest: String = "HealthAdv connects community leaders to members who may benefit from a check-in";
 
   constructor() {}
 }
