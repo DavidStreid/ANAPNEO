fa1e03c2e8c26b14b2ce9ba188cb53d65ee8edfa
David Streid <dcs2153@columbia.edu>
2019-02-24 13:29:09 -0500

    Refactoring init logic

--- projects/anapneo-lib/src/services/login.service.ts
+++ projects/anapneo-lib/src/services/login.service.ts
@@ -9,26 +9,30 @@ import ResponseHandlerUtil from '../utils/services/responseHandler.util';
 @Injectable()
 export class LoginService {
   private loggingEnabled: boolean = false;
+  anapneoService: String;
 
   private responseHandlerUtil: ResponseHandlerUtil;
 
   constructor(private http: HttpClient){
+    this.init();
+  }
+
+  init(){
+    this.anapneoService = environment['anapneoService'] || null;
     this.responseHandlerUtil = new ResponseHandlerUtil();
   }
 
-  login(userId: String, pwd: String) : Observable<Object>{
+  public login(userId: String, pwd: String) : Observable<Object>{
     // TODO - Add logging util
     if( this.loggingEnabled ) console.log( "loginService::login" );
 
-    const anapneoService = environment['anapneoService'] || null;
-
     // TODO - Error handling/backup logic
-    if( ! anapneoService ){
+    if( ! this.anapneoService ){
       const err = 'Anapneo service url is not defined in config';
       return Observable.create( (observer) => { observer.error(err) } );
     }
 
-    const url = `${anapneoService}/login`;
+    const url = `${this.anapneoService}/login`;
     const body = { userId, pwd };
 
     return this.http.post( url, body ).pipe(
@@ -36,8 +40,7 @@ export class LoginService {
         const success = res['success'] || false;
         if( success && res['token'] != null ){
           console.log('Successful login');
-          const token = res[ 'token' ];
-          return { success: true, token };
+          return res;
         } else {
           const status = res['status'] || 'Status unknown';
           return { success: false, status };
