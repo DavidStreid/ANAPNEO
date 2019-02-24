d9d3c35deef450fc1ccb1bab9356e3da1d6d55b4
David Streid <dcs2153@columbia.edu>
2019-02-24 13:28:19 -0500

    Updating AnapneoComponent Tests

--- projects/anapneo-lib/src/anapneo/anapneo.component.spec.ts
+++ projects/anapneo-lib/src/anapneo/anapneo.component.spec.ts
@@ -1,26 +1,21 @@
+import { Component } from '@angular/core';
 import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { AnapneoComponent } from './anapneo.component';
 import { SideBarComponent } from '../sidebar/sidebar.component';
-import { UserProfileService } from '../userProfile/userProfile.service';
-
-// TODO - Replace with real router tests
-import { RouterTestingModule } from '@angular/router/testing';
+import { LoginComponent } from '../loginComponent/login.component';
+import { RouterTestingModule } from '@angular/router/testing';  // TODO - Replace with real router tests
+import { By } from '@angular/platform-browser';
 
 describe('AnapneoComponent', () => {
   let component: AnapneoComponent;
   let fixture: ComponentFixture<AnapneoComponent>;
-  let userProfileServiceSpy: jasmine.SpyObj<UserProfileService>;
 
   beforeEach(async(() => {
-    const spy = jasmine.createSpyObj('UserProfileService', ['getCoordinates']);
-
     TestBed.configureTestingModule({
       imports: [ RouterTestingModule ],
-      declarations: [ AnapneoComponent, SideBarComponent ],
-      providers: [ { provide: UserProfileService, useValue: spy } ]
+      declarations: [ AnapneoComponent, SideBarComponent, MockLoginComponent ],
+      providers: []
     }).compileComponents();
-
-    userProfileServiceSpy = TestBed.get(UserProfileService);
   }));
 
   beforeEach(() => {
@@ -38,4 +33,43 @@ describe('AnapneoComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     // TODO - Add test on load
   });
+
+  it('should render userLogin by default', () => {
+    expect(component.validLogin).toBe(false);   // Should be initialized to false until login event flips
+
+    const loginComponent = fixture.debugElement.query(By.css('user-login'));
+    expect(loginComponent).toBeTruthy();
+  });
+
+  it('should render userLogin by default', () => {
+    // Should be initialized to false until login event flips
+    expect(component.validLogin).toBe(false);
+
+    const loginComponent = fixture.debugElement.query(By.css('user-login'));
+    expect(loginComponent).toBeTruthy();
+  });
+
+  it('should render content if in validLogin state', () => {
+    // Should be initialized to false until login event flips
+    expect(component.validLogin).toBe(false);
+
+    // Trigger Changes
+    component.validLogin = true;
+    fixture.detectChanges();
+
+    const loginComponent = fixture.debugElement.query(By.css('user-login'));
+    expect(loginComponent).toBeNull();
+
+    const sideBarComponent = fixture.debugElement.query(By.css('sidebar'));
+    expect(sideBarComponent).toBeTruthy();
+  });
 });
+
+
+// Mock LoginComponent
+@Component({
+  selector: 'user-login',
+  template: ''
+})
+export class MockLoginComponent {}
+
