6a6427c4006601761765b67585d55fea240285af
David Streid <dcs2153@columbia.edu>
2019-02-24 16:36:27 -0500

    Adding DOM init test

--- projects/anapneo-lib/src/appointmentTrackerComponent/calendarDateComponent/calendar-date.component.spec.ts
+++ projects/anapneo-lib/src/appointmentTrackerComponent/calendarDateComponent/calendar-date.component.spec.ts
@@ -1,12 +1,36 @@
 import { Component }              from '@angular/core';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { CalendarDateComponent }  from './calendar-date.component';
 import { By }                     from '@angular/platform-browser';
 
 describe('CalendarDate Component', () => {
   let component: CalendarDateComponent;
+  let fixture: ComponentFixture<CalendarDateComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [  ],
+      declarations: [ CalendarDateComponent ],
+      providers: []
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CalendarDateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   beforeEach(() => {
     component = new CalendarDateComponent();
+  });
+
+  it( 'should have a date and month div on init', () => {
+    const calendarDayDiv = fixture.debugElement.query(By.css('.calendar-day'));
+    expect( calendarDayDiv ).toBeTruthy();
+
+    const calendarMonthDiv = fixture.debugElement.query(By.css('.calendar-month'));
+    expect( calendarMonthDiv ).toBeTruthy();
   });
 
   it('displayMonth should return the right string', () => {
